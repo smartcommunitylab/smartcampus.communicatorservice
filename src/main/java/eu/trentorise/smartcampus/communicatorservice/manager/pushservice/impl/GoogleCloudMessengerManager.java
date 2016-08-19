0fb97bf0e5be0b04d963ff12d19cd30fc119a02f
sayservice <devel@sayservice.it>
2016-08-19 09:47:04 +0200

    remove notification from GSM message

--- src/main/java/eu/trentorise/smartcampus/communicatorservice/manager/pushservice/impl/GoogleCloudMessengerManager.java
+++ src/main/java/eu/trentorise/smartcampus/communicatorservice/manager/pushservice/impl/GoogleCloudMessengerManager.java
@@ -131,9 +131,9 @@ public class GoogleCloudMessengerManager implements PushServiceCloud {
 				message.addData("title", notification.getTitle());
 				
 				message.priority(Priority.HIGH);
-				com.google.android.gcm.server.Notification.Builder builder = new com.google.android.gcm.server.Notification.Builder("");
-				builder.title(notification.getTitle()).body(notification.getDescription());
-				message.notification(builder.build());
+//				com.google.android.gcm.server.Notification.Builder builder = new com.google.android.gcm.server.Notification.Builder("");
+//				builder.title(notification.getTitle()).body(notification.getDescription());
+//				message.notification(builder.build());
 				
 
 				List<String> regIds = new ArrayList<String>();
